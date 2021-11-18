191f6fc54823a447aafd4fae006baf6c1535807c
Nikita Mashei <nikmashej@gmail.com>
2021-11-18 20:14:40 +0300

    init project

---
+++ src/main/java/dev/mashei/featuretoggledemo/FeatureToggleDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.mashei.featuretoggledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureToggleDemoApplication.class, args);
+	}
+
+}
